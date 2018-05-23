f64393e86672dd93b8f66738f57558a92001385d
cy <1655770928@qq,com>
2018-05-23 15:14:13 +0800

    用户注册

---
+++ lqpay/src/org/lanqiao/service/yonghuService.java
@@ -0,0 +1,32 @@
+package org.lanqiao.service;
+
+public class yonghuService {
+  private String sname;
+  private String pwd;
+  private int phonenum;
+  private String emailadd;
+public String getSname() {
+	return sname;
+}
+public void setSname(String sname) {
+	this.sname = sname;
+}
+public String getPwd() {
+	return pwd;
+}
+public void setPwd(String pwd) {
+	this.pwd = pwd;
+}
+public int getPhonenum() {
+	return phonenum;
+}
+public void setPhonenum(int phonenum) {
+	this.phonenum = phonenum;
+}
+public String getEmailadd() {
+	return emailadd;
+}
+public void setEmailadd(String emailadd) {
+	this.emailadd = emailadd;
+}
+}
